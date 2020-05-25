aefbf975d587424728488cf83e0ec06666936a76
Vinayak Kulkarni <inbox.vinayak@gmail.com>
2020-05-26 03:51:32 +0530

    docs(example): add component usage example 📂

---
+++ example/App.svelte
@@ -0,0 +1,28 @@
+<script>
+	import SOffline from '../src/SOffline.svelte';
+	const handleNetworkChange = ({ detail }) => {
+		console.log('event details: ', detail);
+	}
+</script>
+
+<SOffline
+	pingUrl="https://bitly.com"
+	on:detectedCondition={handleNetworkChange}>
+		<span slot="online" class="online">
+			😊
+		</span>
+		<span slot="offline" class="offline">
+			🤕
+		</span>
+</SOffline>
+
+<style>
+.online {
+	font-size: 10rem;
+	text-align: center;
+}
+.offline {
+	font-size: 10rem;
+	text-align: center;
+}
+</style>
