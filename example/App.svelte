e1ff84f23e331eb980ba9aec847dfb5320bd0b94
Vinayak Kulkarni <inbox.vinayak@gmail.com>
2020-05-28 22:49:02 +0530

    style(lint): update example & source 💅

--- example/App.svelte
+++ example/App.svelte
@@ -1,28 +1,24 @@
 <script>
-	import SOffline from '../src/SOffline.svelte';
-	const handleNetworkChange = ({ detail }) => {
-		console.log('event details: ', detail);
-	}
+  import SOffline from '../src/SOffline.svelte';
+  const handleNetworkChange = ({ detail }) => {
+    console.log('event details: ', detail);
+  };
 </script>
 
-<SOffline
-	pingUrl="https://bitly.com"
-	on:detectedCondition={handleNetworkChange}>
-		<span slot="online" class="online">
-			😊
-		</span>
-		<span slot="offline" class="offline">
-			🤕
-		</span>
-</SOffline>
-
 <style>
-.online {
-	font-size: 10rem;
-	text-align: center;
-}
-.offline {
-	font-size: 10rem;
-	text-align: center;
-}
+  .online {
+    font-size: 10rem;
+    text-align: center;
+  }
+  .offline {
+    font-size: 10rem;
+    text-align: center;
+  }
 </style>
+
+<SOffline
+  pingUrl="https://bitly.com"
+  on:detectedCondition={handleNetworkChange}>
+  <span slot="online" class="online">😊</span>
+  <span slot="offline" class="offline">🤕</span>
+</SOffline>
--- src/SOffline.svelte
+++ src/SOffline.svelte
@@ -1,27 +1,30 @@
 <script>
-	import Ping from 'ping.js';
-	import { createEventDispatcher } from 'svelte';
-	// constants
-	const emit = createEventDispatcher();
+  import Ping from 'ping.js';
+  import { createEventDispatcher } from 'svelte';
+  // constants
+  const emit = createEventDispatcher();
   // props
   export let pingUrl = 'https://google.com';
-	// reactive variables - computed + data
-	$: isOnline = navigator.onLine || false; // data variable
-	// methods
-	const updateOnlineStatus = () => {
-		const p = new Ping();
-		p.ping(pingUrl, (err) => {
-			if (err || !navigator.onLine) {
-				isOnline = false;
-			} else {
-				isOnline = true;
-			}
-			emit('detectedCondition', { online: isOnline });
-		});
-	};
+  // reactive variables - computed + data
+  $: isOnline = navigator.onLine || false; // data variable
+  // methods
+  const updateOnlineStatus = () => {
+    const p = new Ping();
+    p.ping(pingUrl, (err) => {
+      if (err || !navigator.onLine) {
+        isOnline = false;
+      } else {
+        isOnline = true;
+      }
+      emit('detectedCondition', { online: isOnline });
+    });
+  };
 </script>
 
-<svelte:window on:online={updateOnlineStatus} on:offline={updateOnlineStatus} on:load={updateOnlineStatus}/>
+<svelte:window
+  on:online={updateOnlineStatus}
+  on:offline={updateOnlineStatus}
+  on:load={updateOnlineStatus} />
 
 {#if isOnline}
   <slot name="online">
