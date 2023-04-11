3207a3c9f36af362d36aa38151c1b14db039dc78
Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>
2023-04-12 02:38:49 +0530

    fix: remove buildCommand
    Signed-off-by: Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>

--- ship.config.mjs
+++ ship.config.mjs
@@ -1,5 +1,4 @@
 export default {
   installCommand: () => 'npm i --prefer-offline --no-audit --omit=optional',
-  buildCommand: () => 'npm run package',
   publishCommand: () => 'cd dist && npm publish --tag latest',
 };
